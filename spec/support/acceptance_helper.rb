1539803b440ea93240a4a71a3a9698d4d4576e3f
Cezary Baginski <cezary@chronomantic.net>
2015-06-12 13:01:34 +0200

    remove Celluloid

--- spec/support/acceptance_helper.rb
+++ spec/support/acceptance_helper.rb
@@ -194,7 +194,7 @@ class ListenerWrapper
       yield
 
       # Polling sleep (default: 1s)
-      adapter = @listener.sync(:adapter)
+      adapter = @listener.send(:adapter)
       sleep(adapter.options.latency) if adapter.is_a?(Listen::Adapter::Polling)
 
       # Lag should include:
