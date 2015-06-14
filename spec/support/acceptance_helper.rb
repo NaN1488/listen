21086444a30edecc043de143e4fd3d16d1eef817
Cezary Baginski <cezary@chronomantic.net>
2015-06-14 22:58:10 +0200

    let acceptance specs bypass Listen API

--- spec/support/acceptance_helper.rb
+++ spec/support/acceptance_helper.rb
@@ -194,7 +194,8 @@ class ListenerWrapper
       yield
 
       # Polling sleep (default: 1s)
-      adapter = @listener.send(:adapter)
+      backend = @listener.instance_variable_get(:@backend)
+      adapter = backend.instance_variable_get(:@adapter)
       sleep(adapter.options.latency) if adapter.is_a?(Listen::Adapter::Polling)
 
       # Lag should include:
